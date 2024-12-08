b881c30a842d600be06486f766ad229d03a92bfd
kyb_home <kyb07@o2o.kr>
2024-12-08 11:39:31 +0900

    유저별 챗봇 인스턴스 생성 하도록.

--- src/app/present/bot/chat_cog.py
+++ src/app/present/bot/chat_cog.py
@@ -1,13 +1,22 @@
 import discord
 from discord.ext import commands
 
-from src.dependencies import gpt, auth_use_case
+from src.dependencies import auth_use_case, env
 from src.domain.model import UserGrantEnum
+from src.infra.gpt import GPT
 
 
 class ChatCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.gpt_dict = {}
+
+    def get_gpt(self, user_id: str) -> GPT:
+        gpt = self.gpt_dict.get(user_id, None)
+        if gpt is None:
+            self.gpt_dict[user_id] = GPT(env=env)
+            return self.gpt_dict[user_id]
+        return gpt
 
     @commands.command(name='chat', aliases=['c'], help='{chat, c} 챗봇 기능')
     async def chat(self, ctx, *, user_input: str):
@@ -15,6 +24,7 @@ class ChatCog(commands.Cog):
             user_id=ctx.author.name,
             grant_enum=UserGrantEnum.USE_CHAT_BOT
         )
+        gpt = self.get_gpt(user_input)
         gpt.add_message(role='user', message=user_input)
         ret = gpt.compilation()
         await ctx.send(ret)
